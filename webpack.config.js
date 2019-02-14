46afea5dd926f0c8de5f8c6c56906a4e917592c2
nkhanhng <nkhanhng97@gmail.com>
2019-02-14 14:51:45 +0700

    package

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use:  ["babel-loader"]
+      },
+
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+}
